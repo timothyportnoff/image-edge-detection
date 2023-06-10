4de976d21cb8939c2449a8deffb8c7b70d772092
timothyportnoff <timothyportnoff@gmail.com>
2023-06-10 05:44:41 -0700

    Moved function declarations to top of file and moved function definitions to bottom of file

--- prewitt_filter.h
+++ prewitt_filter.h
@@ -1,11 +1,60 @@
 #include <vector>
 #include <cmath>
-//#include <iostream> FIXME remove?
 
 //Global constants
 const int R = 0, G = 1, B = 2, T = 3;
 const int num_colors = 3;
-const int magnitude_threshold = 120;
+const int magnitude_threshold = 50;
+
+void set_pixel(std::vector<std::vector<std::vector<int>>> &vec, size_t &i, size_t &j, int r, int g, int b);
+std::vector<int> prewitt_sum_horizontal(std::vector<std::vector<std::vector<int>>> &vec, size_t &i, size_t &j); 
+std::vector<int> prewitt_sum_vertical(std::vector<std::vector<std::vector<int>>> &vec, size_t &i, size_t &j);
+
+void prewitt_filter(std::vector<std::vector<std::vector<int>>> &input) {
+	//Get the number of rows, columns, and colors in vector and vet
+	size_t rows = input.size();
+	if (!rows) exit(1);
+	size_t cols = input[0].size();
+	if (!cols) exit(1);
+	size_t num_colors = input[0][0].size();
+	if (!num_colors) exit(1);
+
+	//Create a copy so that we're not fucking with our original image. This issue was driving me bonkers lol
+	std::vector<std::vector<std::vector<int>>> output(rows, std::vector<std::vector<int>>(cols, std::vector<int>(num_colors)));
+
+	//Do the image filtering on every row and column in this image
+	for (size_t i = 0; i < rows; i++) {
+		for (size_t j = 0; j < cols; j++) {
+			//Don't even fuck with any of the edge cases. Deal with that shit later
+			if(i == 0) continue;
+			if(i == rows - 1) continue;
+			if(j == 0) continue;
+			if(j == cols - 1) continue;
+
+			//Initially pixel is NOT an edge
+			bool is_edge = 0;
+
+			//Save rgb for better readability
+			int r = input[i][j][R];
+			int g = input[i][j][G];
+			int b = input[i][j][B];
+
+			//Red, blue and green channel totals of our prewitt kernel 
+			std::vector<int> horizontal_totals = prewitt_sum_horizontal(input, i, j);
+			std::vector<int> vertical_totals 	= prewitt_sum_vertical(input, i, j);
+			std::vector<int> total_totals 	= {0,0,0};
+			for (size_t k = 0; k < total_totals.size(); k++) {
+				total_totals[k] = sqrt(pow(horizontal_totals[k], 2) + pow(vertical_totals[k], 2));
+				if (total_totals[k] >= magnitude_threshold) is_edge = 1;
+			}
+
+			//
+			if (is_edge) set_pixel(output, i, j, 30, 30, 30);
+			else set_pixel(output, i, j, r, g, b);
+		}
+	}
+	input = output;
+}
 
 void set_pixel(std::vector<std::vector<std::vector<int>>> &vec, size_t &i, size_t &j, int r, int g, int b) {
 	vec[i][j][R]=r;
@@ -65,46 +114,4 @@ std::vector<int> prewitt_sum_vertical(std::vector<std::vector<std::vector<int>>>
 	b_total -= vec[i+1][j+1][B];
 
 	return {r_total,g_total,b_total};
-}
-
-void prewitt_filter(std::vector<std::vector<std::vector<int>>> &vec) {
-	//Get the number of rows, columns, and colors in vector and vet
-	size_t rows = vec.size();
-	if (!rows) exit(1);
-	size_t cols = vec[0].size();
-	if (!cols) exit(1);
-	size_t num_colors = vec[0][0].size();
-	if (!num_colors) exit(1);
-
-	//Do the image filtering on every row and column in this image
-	for (size_t i = 0; i < rows; i++) {
-		for (size_t j = 0; j < cols; j++) {
-			//Don't even fuck with any of the edge cases. Deal with that shit later
-			if(i == 0) continue;
-			if(i == rows - 1) continue;
-			if(j == 0) continue;
-			if(j == cols - 1) continue;
-
-			//Reset our totals to 0
-			int r_total = 0;
-			int g_total = 0;
-			int b_total = 0;
-
-			//Assign rgb for better readability
-			int r = vec[i][j][R];
-			int g = vec[i][j][G];
-			int b = vec[i][j][B];
-
-			//Red, blue and green channel totals of our prewitt kernel 
-			std::vector<int> horizontal_totals = prewitt_sum_horizontal(vec, i, j);
-			std::vector<int> vertical_totals 	= prewitt_sum_vertical(vec, i, j);
-			std::vector<int> total_totals 	= {0,0,0};
-			for (int &t : horizontal_totals) 	{ t = t*t; }
-			for (int &t : vertical_totals) 	{ t = t*t; }
-			for (int &t : total_totals) 		{ t = t*t; }
-			for (size_t k = 0; k < total_totals.size(); k++) total_totals[k] = sqrt(horizontal_totals[k] + vertical_totals[k]);
-			for (size_t k = 0; k < total_totals.size(); k++) if (total_totals[k] >= magnitude_threshold) set_pixel(vec, i, j, 30, 30, 30);
-			//else if (dot[i % dot.size()][j % dot.size()] == 'R') {
-		}
-	}
 }
